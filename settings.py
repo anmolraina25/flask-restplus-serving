26b2a8f02b94196c692232dd39092f28afdff595
anmolraina25 <36219237+anmolraina25@users.noreply.github.com>
2018-05-04 15:47:18 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+# Flask settings
+DEFAULT_FLASK_SERVER_NAME = '192.168.1.5'
+DEFAULT_FLASK_SERVER_PORT = '5000'
+DEFAULT_FLASK_DEBUG = True  # Do not use debug mode in production
+
+# Flask-Restplus settings
+RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'full'
+RESTPLUS_VALIDATE = True
+RESTPLUS_MASK_SWAGGER = False
+RESTPLUS_ERROR_404_HELP = False
+
+# GAN client settings
+DEFAULT_TF_SERVER_NAME = '172.17.0.1'
+DEFAULT_TF_SERVER_PORT = 3000
+GAN_MODEL_NAME = 'exp_model'
+GAN_MODEL_INPUTS_KEY = 'images'
